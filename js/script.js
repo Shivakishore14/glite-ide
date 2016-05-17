8d4a3bf5fab63bba9e6883cca3f673d8d49cbc41
shiva kishore <shivakishore14@gmail.com>
2016-05-17 23:48:14 +0530

    editor selection added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
-window.onload = function() {
-//	$("#inputblock").hide();
-//	var ww = $("#prog").width();	
-//	$("#prog").width(ww-ww/20);	
+window.onload = function(){
+var arr = [true,true,true,true];
+var editorarr = ["htmleditor","csseditor","jseditor","resouter"];
+var currentold = [4,0];
+var sizeclass = ["col-sm-","col-md-","col-lg-"];
+function resize(){
+	var i,j;
+	for(i=0;i<4;i++)
+		for(j=0;j<3;j++){
+			$("#"+editorarr[i]).removeClass(sizeclass[j]+Math.round((12/currentold[1])).toString());
+			$("#"+editorarr[i]).addClass(sizeclass[j]+Math.round((12/currentold[0])).toString());
+		}
+			
 }
-$("#uploadfile , #uploadtext").on('click',function(event) {
-	$("#myfile").trigger("click");
-});
-$("#mybtn").click(function(){
-	var a = $("#myfile").val();
-	$("#status").html("Uploading...");
-	$("#myfile").upload("files/hello.py",function(success){
-		$("#status").html(a+" uploaded");
-	},$("#prog"));
-});
+function handle( i){
+	arr[i] = !arr[i];
+	if(!arr[i]) {
+		$("#"+editorarr[i]).hide();
+		currentold[1] = currentold[0];
+		currentold[0]--;
+	} else {
+		$("#"+editorarr[i]).show();
+		currentold[1] = currentold[0];
+                currentold[0]++;
+	}
+	resize();
+}
+$("#htmlbtn").on('click', function() {
+                handle(0);
+        }
+);
+$("#cssbtn").on('click',function() {
+                handle(1);
+        }
+);
+$("#jsbtn").on('click', function() {
+                handle(2);
+        }
+);
+$("#outputbtn").on('click', function() {
+                handle(3);
+        }
+);
+}
 $("#result").on('click',function(event){
 	var html = $("#htmlcode").val();
 	var css = $("#csscode").val();
