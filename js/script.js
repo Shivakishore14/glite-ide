04a117ce1a0e83428a9e3bd1909a4e1dd270ee67
shiva kishore <shivakishore14@gmail.com>
2016-05-18 22:45:29 +0530

    added horizontal result

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 window.onload = function(){
 	var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+	var th = $("#test").height();
+	$("#test").hide();
+	function init(){
+		var h = $("#htmltitle").offset().top;
+		htmleditor.setSize("100%",th-h-h/2);
+		csseditor.setSize("100%",th-h-h/2);
+		jseditor.setSize("100%",th-h-h/2);
+		$("#result1").height(th-h-h/2);
+	
+	}
+	var withbox = true;
 	var arr = [true,true,true,true];
 	var editorarr = ["htmleditor","csseditor","jseditor","resouter"];
 	var currentold = [4,0];
@@ -14,14 +25,22 @@ window.onload = function(){
 	}
 	function handle( i){
 		arr[i] = !arr[i];
-		if(!arr[i]) {
-			$("#"+editorarr[i]).hide();
-			currentold[1] = currentold[0];
-			currentold[0]--;
-		} else {
-			$("#"+editorarr[i]).show();
-			currentold[1] = currentold[0];
-        	        currentold[0]++;
+		if(i != 3 || withbox){
+			if(!arr[i]) {
+				$("#"+editorarr[i]).hide();
+				currentold[1] = currentold[0];
+				currentold[0]--;
+			} else {
+				$("#"+editorarr[i]).show();
+				currentold[1] = currentold[0];
+        		        currentold[0]++;
+			}
+		}else{
+			if(!arr[i])
+				$("#res2outer").hide();
+			else
+				$("#res2outer").show();	
+			init();		
 		}
 		resize();
 	}	
@@ -37,23 +56,17 @@ window.onload = function(){
 	$("#outputbtn").on('click', function() {
                 handle(3);
         });
-	require(["../codemirror/lib/codemirror", "../codemirror/mode/htmlmixed/htmlmixed"] ,function(CodeMirror) {
-		window.htmleditor = CodeMirror.fromTextArea(document.getElementById("htmlcode"), {
-			lineNumbers: true,
-			mode: "htmlmixed"
-  		});
-	});
-	require(["../codemirror/lib/codemirror", "../codemirror/mode/css/css"] ,function(CodeMirror) {
-  		window.csseditor = CodeMirror.fromTextArea(document.getElementById("csscode"), {
-    			lineNumbers: true,
-			mode: "css"
+	var htmleditor = CodeMirror.fromTextArea(document.getElementById("htmlcode"), {
+		lineNumbers: true,
+		mode: "htmlmixed"
+  	});
+	var csseditor = CodeMirror.fromTextArea(document.getElementById("csscode"), {
+    		lineNumbers: true,
+		mode: "css"
 		});
-	});
-	require(["../codemirror/lib/codemirror", "../codemirror/mode/javascript/javascript"] ,function(CodeMirror) {
-		window.jseditor = CodeMirror.fromTextArea(document.getElementById("jscode"), {
-			lineNumbers: true,
-			mode: "javascript"
-		});
+	var jseditor = CodeMirror.fromTextArea(document.getElementById("jscode"), {
+		lineNumbers: true,
+		mode: "javascript"
 	});
 	$("#result").on('click',function(event){
 		htmleditor.save();
@@ -66,4 +79,7 @@ window.onload = function(){
 		var data_url = "data:text/html;charset=utf-8;base64," + Base64.encode(result);
         	document.getElementById("result1").src = data_url;	
 	});
+	init();
+	
 }
+
